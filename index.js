72bcfb1746a4605f92fb77e513b7476dc0512119
Nathaniel Watson <nathaniel@nwatson.nz>
2015-12-21 21:54:04 +1300

    Reworked the stvChart completely to better follow the reusable charts approach. Changed each count bar to be scaled based on the total votes, rather than the valid votes in the round/count.
    Added informal and invalid vote counts to the data.

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function ready(fn) {
 }
 
 ready(function() {
+	var margin = {top: 0, right: 0, bottom: 0, left: 0},
+		width = 800 - margin.left - margin.right,
+		height = 600 - margin.top - margin.bottom;
+
+	var chartElement = d3.select('#chart').append('svg')
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
 	d3.json('data.json', function(error, json) {
 		if (!error) {
 
@@ -16,7 +26,54 @@ ready(function() {
 				d.totals = d3.map(d.totals, function(f) { return f.key });
 			});
 
-			stvChart('#chart', json);
+			var chart = stvChart()
+				.width(width)
+				.height(height)
+				.counts(json.counts)
+				.layout();
+
+
+			var colors = d3.scale.category10()
+				.domain(json.choices.map( function(d) { return d.key }));
+
+			var countGroups = chartElement.selectAll('.count')
+				.data(chart.counts())
+				.enter()
+				.append('g')
+				.attr('class', 'count');
+
+			countGroups.selectAll('rect')
+				.data(function(d) { return d.totals.values() })
+				.enter()
+				.append('rect')
+				.attr('class', 'flag-node')
+				.attr('width', function(d) { return d.geo.dx })
+				.attr('height', function(d) { return d.geo.dy })
+				.attr('y', function(d) { return d.geo.y })
+				.attr('x', function(d) { return d.geo.x })
+				.attr('fill', function(d) { return colors(d.key) });
+
+
+			var flowsGroup = chartElement.append('g')
+				.attr('class', 'flows');
+
+			flowsGroup.selectAll('.flow')
+				.data(chart.flows())
+				.enter()
+				.append('path')
+				.attr('class', 'flow')
+				.attr('d', function(d) {
+					var dist = d.geo.x1 - d.geo.x0;
+
+					return 'M' +  d.geo.x0 + ',' + d.geo.y0
+						+ ' C' + (d.geo.x0 + dist/2) + ',' + d.geo.y0
+						+ ' ' + (d.geo.x1 - dist/2) + ',' + d.geo.y1
+						+ ' ' + d.geo.x1 + ',' + d.geo.y1;
+				})
+				.style('stroke', '#333')
+				.style('stroke-width', function(d) { return d.geo.width })
+				.style('opacity', '0.2')
+				.style('fill', 'none');
 		}
 	});
 });
--- stvChart.js
+++ stvChart.js
@@ -1,168 +1,123 @@
 /* exported stvChart */
 /* global stvChart d3  */
 
-function stvChart(selector, data, opts) {
-	opts = opts || {};
+function stvChart() {
+	var width = 800,
+		height = 600,
+		nodeSizeRatio = 0.2,
+		counts = [],
+		flows = [];
 
-	var margin = opts.margin || {top: 0, right: 0, bottom: 0, left: 0},
-		width = opts.width || 800 - margin.left - margin.right,
-		height = opts.height || 600 - margin.top - margin.bottom,
-		nodePadding = opts.nodePadding || 0,
-		nodeFlowRatio = opts.nodeFlowRatio || 0.2;
+	function chart() {}
 
-	var chart = d3.select(selector).append('svg')
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
-		.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-	var xScale = d3.scale.ordinal()
-		.domain(data.counts.map(function(d) { return d.name }))
-		.rangeBands([margin.left, width], 1 - nodeFlowRatio, 0);
-
-	var yScale = d3.scale.linear()
-		.domain([0, 1])
-		.range([margin.top, height]);
-
-	function computeNodeGeometry() {
-		data.counts.forEach(function(count){
-			var y0 = 0;
-
-			count.totals.forEach(function(k, v) {
-				// Add raw y-values - the next loop normalizes them
-				v.geo = {
-					y: y0,
-					dy: v.votes
-				};
-
-				y0 += v.votes;
-			});
-
-			count.totals.forEach(function(k, v) {
-				v.geo.x = xScale(count.name);
-				v.geo.dx = xScale.rangeBand(count.name);
-
-				v.geo.y = yScale(v.geo.y/y0);
-				v.geo.dy = yScale(v.geo.dy/y0);
-
-				// TODO: Combine flowGeo calc into here so that stuff like y0 can be used to do positioning
-			});
-		});
+	chart.width = function(val) {
+		if (!arguments.length) { return width }
+		width = val;
+		return chart;
 	}
 
-	function computeFlowGeometry() {
-		var flows = [];
+	chart.height = function(val) {
+		if (!arguments.length) { return height }
+		height = val;
+		return chart;
+	}
 
-		for (var i = 1; i < data.counts.length; i++) {
-			var count = data.counts[i];
-			var prevCount = data.counts[i-1];
+	chart.nodeSizeRatio = function(val) {
+		if (!arguments.length) { return nodeSizeRatio }
+		nodeSizeRatio = val;
+		return chart;
+	}
 
-			var prevKeys = prevCount.totals.keys();
+	chart.counts = function(val) {
+		if (!arguments.length) { return counts }
+		counts = val;
+		return chart;
+	}
 
-			var eliminated;
-
-			for (var j = 0; j < prevKeys.length; j++) {
-				if (!count.totals.has(prevKeys[j])) {
-					eliminated = prevCount.totals.get(prevKeys[j]);
-					break;
-				}
-			}
-
-			count.totals.forEach(function(k, v) {
-				var prevTotal = prevCount.totals.get(k);
-				var newVotes = v.votes - prevTotal.votes;
-
-				flows.push(new Flow(prevTotal.votes, prevTotal, v));
-
-				if (newVotes > 0) {
-					flows.push(new Flow(newVotes, eliminated, v));
-				}
-			});
-
-			/*
-				figure out which was eliminated (compare arrays)
-
-				for each flag's total:
-					figure out how many votes it gained
-					add a flow to the same flag in the previous count
-					add a flow to the eliminated flag with a size equal to the votes gained
-			*/
-		}
-
+	chart.flows = function() {
 		return flows;
 	}
 
-	function Flow(votes, from, to) {
+	chart.layout = function() {
+		var xScale = d3.scale.ordinal()
+			.domain(counts.map(function(d) { return d.name }))
+			.rangeBands([0, width], 1 - nodeSizeRatio, 0);
+
+		var yScale = d3.scale.linear()
+			.domain([0, d3.sum(counts[0].totals.values(), function(d) { return d.votes })])
+			.range([0, height]);
+
+		computeNodeGeometry(xScale, yScale);
+
+		return chart;
+	}
+
+	function computeNodeGeometry(xScale, yScale) {
+		counts.forEach(function(count, i) {
+			var x = xScale(count.name);
+			var dx = xScale.rangeBand(count.name);
+
+			var yAcummulated = 0;
+
+			// Add geometry data to each candidate in this count/round
+			count.totals.forEach(function(k, v) {
+				v.geo = {
+					x: x,
+					dx: dx,
+
+					y: yScale(yAcummulated),
+					dy: yScale(v.votes)
+				}
+
+				yAcummulated += v.votes;
+			});
+
+			// Add flows going from each candidate in the current count back to the previous count.
+			if (i > 0) {
+				var prevCount = counts[i-1];
+				var prevCountKeys = prevCount.totals.keys();
+				var eliminated;
+
+				for (var j = 0; j < prevCountKeys.length; j++) {
+					// Figure out which candidate has been removed since the previous count
+					if (!count.totals.has(prevCountKeys[j])) {
+						eliminated = prevCount.totals.get(prevCountKeys[j]);
+						break;
+					}
+				}
+
+				var elimVoteOffset = 0;
+
+				count.totals.forEach(function(k, v) {
+					var prevTotal = prevCount.totals.get(k);
+					var newVotes = v.votes - prevTotal.votes;
+
+					flows.push(new Flow(prevTotal.votes, yScale, 0, 0, prevTotal, v));
+
+					if (newVotes > 0) {
+						flows.push(new Flow(newVotes, yScale, prevTotal.votes, elimVoteOffset, eliminated, v));
+						elimVoteOffset += newVotes;
+					}
+				});
+			}
+		});
+	}
+
+	function Flow(votes, yScale, fromVoteOffset, toVoteOffset,  from, to) {
+		var width = yScale(votes);
+
 		this.from = from;
 		this.to = to;
 		this.votes = votes;
 
 		this.geo = {
-			xL: from.geo.x + from.geo.dx,
-			xR: to.geo.x,
-			yTL: from.geo.y,
-			yBL: from.geo.y + votes/from.votes * from.geo.dy,
-			yTR: to.geo.y,
-			yBR: to.geo.y + votes/to.votes * to.geo.dy
+			x0: from.geo.x + from.geo.dx,
+			x1: to.geo.x,
+			y0: from.geo.y + (width / 2) + yScale(toVoteOffset),
+			y1: to.geo.y + (width / 2) + yScale(fromVoteOffset),
+			width: width
 		}
 	}
 
-	// DRAWING STUFF TODO: Move elsewhere
-
-	computeNodeGeometry();
-	var flows = computeFlowGeometry();
-
-	var colors = d3.scale.category10()
-		.domain(data.choices.map( function(d) { return d.key }));
-
-	var count = chart.selectAll('.count')
-		.data(data.counts)
-		.enter()
-		.append('g')
-		.attr('class', 'count')
-		.attr('transform', function(d) { return 'translate(' + xScale(d.name) + ', 0)' });
-
-	count.selectAll('rect')
-		.data(function(d) { return d.totals.values() })
-		.enter()
-		.append('rect')
-		.attr('class', 'flag-node')
-		.attr('width', function(d) { return d.geo.dx })
-		.attr('height', function(d) { return d.geo.dy })
-		.attr('y', function(d) { return d.geo.y })
-		.attr('fill', function(d) { return colors(d.key) });
-
-	var flowsGroup = chart.append('g')
-		.attr('class', 'flows');
-
-	flowsGroup.selectAll('.flow')
-		.data(flows)
-		.enter()
-		.append('path')
-		.attr('class', 'flow')
-		.attr('d', function(d) {
-			var dist = d.geo.xR - d.geo.xL;
-
-			var outerCurveX = dist/1.5;
-			var innerCurveX = Math.max(dist/2, dist/1.5 * (1 - ((d.geo.yBL - d.geo.yTL)/dist)));
-
-			if (d.geo.yTL < d.geo.yTR) {
-				var t = outerCurveX;
-				outerCurveX = innerCurveX;
-				innerCurveX = t;
-			}
-
-			return 'M' +  d.geo.xL + ',' + d.geo.yTL
-				+ ' C' + (d.geo.xL + innerCurveX) + ',' + d.geo.yTL
-				+ ' ' + (d.geo.xR - outerCurveX) + ',' + d.geo.yTR
-				+ ' ' + d.geo.xR + ',' + d.geo.yTR
-				+ ' L' + d.geo.xR + ',' + d.geo.yBR
-				+ ' C' + (d.geo.xR - innerCurveX) + ',' + d.geo.yBR
-				+ ' ' + (d.geo.xL + outerCurveX) + ',' + d.geo.yBL
-				+ ' ' + d.geo.xL + ',' + d.geo.yBL
-				+ ' z';
-		})
-		.style('fill', '#333')
-		.style('opacity', '0.2');
-
+	return chart;
 }
